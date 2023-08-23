a5720a6292b42b1725e0b14ab4c00e5c0648c761
Stures <89753508+Stur3s@users.noreply.github.com>
2023-08-24 01:05:48 +0200

    reminder.py

--- reminder.py
+++ reminder.py
@@ -1,6 +1,5 @@
 import discord
 from discord import app_commands
-import asyncio
 import calendar
 import time
 ts = calendar.timegm(time.gmtime())
@@ -27,91 +26,22 @@ async def on_ready():
     except Exception as e:
         print(e)
 
-@bot.tree.command(name = "yo", description= "p'tit reminder")
-async def yo_slash(interaction: discord.Interaction):
-    await interaction.response.send_message('tu seras remind dans 10m')
+@bot.tree.command(name = "name", description= "reminder")
+async def name_slash(interaction: discord.Interaction):
+    await interaction.response.send_message('you will remind in <time you want>')
     channel = client.get_channel(str(channelId))
     time.sleep(9*60+45)
-    await interaction.channel.send(f"ton reminder <@" + str(interaction.user.id) + ">")
+    await interaction.channel.send(f"your reminder <@" + str(interaction.user.id) + ">")
     
-@bot.tree.command(name = "effrayé", description= "p'tit reminder")
-async def effrayé_slash(interaction: discord.Interaction):
-    await interaction.response.send_message('tu seras remind dans 20m :scream:')
+#create a custom reminder
+@bot.tree.command(name = "reminder", description= "custom reminder")
+async def rmd_slash(interaction: discord.Interaction, hours: int = None, minutes: int = None, seconds: int = None):
+    x = int(3600)*hours + int(60)* minutes + seconds
+    await interaction.response.send_message(f'you will remind in ', hours, 'h, ', minutes, ' m et', seconds, 's'  )
     channel = client.get_channel(str(channelId))
-    time.sleep(19*60+45)
-    await interaction.channel.send(f"ton reminder <@" + str(interaction.user.id) + ">")
-
-@bot.tree.command(name = "confus", description= "p'tit reminder")
-async def confus_slash(interaction: discord.Interaction):
-    await interaction.response.send_message('tu seras remind dans 50m :confounded:')
-    channel = client.get_channel(str(channelId))
-    time.sleep(49*60+45)
-    await interaction.channel.send(f"ton reminder <@" + str(interaction.user.id) + ">")
-
-@bot.tree.command(name = "gelé", description= "p'tit reminder")
-async def confus_slash(interaction: discord.Interaction):
-    await interaction.response.send_message('tu seras remind dans 1h10 :cold_face:')
-    channel = client.get_channel(str(channelId))
-    time.sleep(69*60+45)
-    await interaction.channel.send(f"ton reminder <@" + str(interaction.user.id) + ">")
-
-@bot.tree.command(name = "affamé", description= "reminder si affamé")
-async def affamé_slash(interaction: discord.Interaction):
-    await interaction.response.send_message('tu seras remind dans 1h30 :drooling_face: ')
-    channel = client.get_channel(str(channelId))
-    time.sleep(89*60+45)
-    await interaction.channel.send(f"ton reminder <@" + str(interaction.user.id) + ">")
-
-@bot.tree.command(name = "endormi", description= "reminder si endormi")
-async def affamé_slash(interaction: discord.Interaction):
-    await interaction.response.send_message('tu seras remind dans 3h10 :sleeping:')
-    channel = client.get_channel(str(channelId))
-    time.sleep(189*60+45)
-    await interaction.channel.send(f"ton reminder <@" + str(interaction.user.id) + ">")
-
-@bot.tree.command(name = "ivre", description= "reminder si ivre")
-async def ivre_slash(interaction: discord.Interaction):
-    await interaction.response.send_message('tu seras remind dans 4h10 :zany_face: ')
-    channel = client.get_channel(str(channelId))
-    time.sleep(249*60+45)
-    await interaction.channel.send(f"ton reminder <@" + str(interaction.user.id) + ">")
-
-@bot.tree.command(name = "blessé", description= "reminder si blessé")
-async def blessé_slash(interaction: discord.Interaction):
-    await interaction.response.send_message('tu seras remind dans 6h10 :head_bandage:')
-    channel = client.get_channel(str(channelId))
-    time.sleep(369*60+45)
-    await interaction.channel.send(f"ton reminder <@" + str(interaction.user.id) + ">")
-
-@bot.tree.command(name = "malade", description= "reminder si malade")
-async def blessé_slash(interaction: discord.Interaction):
-    await interaction.response.send_message('tu seras remind dans 6h10 :nauseated_face: ')
-    channel = client.get_channel(str(channelId))
-    time.sleep(369*60+45)
-    await interaction.channel.send(f"ton reminder <@" + str(interaction.user.id) + ">")
-
-@bot.tree.command(name = "gravement_blessé", description= "reminder si gravement blessé")
-async def grvblessé_slash(interaction: discord.Interaction):
-    await interaction.response.send_message('tu seras remind dans 12h10 :dizzy_face:')
-    channel = client.get_channel(str(channelId))
-    time.sleep(2*369*60+45)
-    await interaction.channel.send(f"ton reminder <@" + str(interaction.user.id) + ">")
-
-@bot.tree.command(name = "prison", description= "reminder si gravement blessé")
-async def prison_slash(interaction: discord.Interaction):
-    await interaction.response.send_message('tu seras remind dans 24h10 :lock:')
-    channel = client.get_channel(str(channelId))
-    time.sleep(4*369*60+45)
-    await interaction.channel.send(f"ton reminder <@" + str(interaction.user.id) + ">")
-
-@bot.tree.command(name = "rmd", description= "ton reminder")
-async def rmd_slash(interaction: discord.Interaction, heures: int = None, minutes: int = None, secondes: int = None):
-    x = int(3600)*heures + int(60)* minutes + secondes
-    await interaction.response.send_message(f'tu seras remind dans', heures, 'h, ', minutes, ' m et', secondes, 's'  )
-    channel = client.get_channel(str(channelId))
-    time.sleep(x)
-    await interaction.channel.send(f"ton reminder <@" + str(interaction.user.id) + ">")
+    time.sleep(<time you want in seconds>)
+    await interaction.channel.send(f"your reminder <@" + str(interaction.user.id) + ">")
 
 
 
-bot.run("MTExNTcwMTMzNTc3NDQ3NDI5MA.GIv1ct.PJXAQfjXihDncicZ9N3-JpCNjNZHUsjktwhWGU")
+bot.run("token of your bot")
